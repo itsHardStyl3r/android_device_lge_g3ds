6cf6aa316abf4874b05289a72d75b0a02a0ea3fa
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-08-15 15:25:06 +0200

    d855: TWRP update
     * building kernel inline
     * allow recovery backup
     * clean device tree
     * remove README.md
     * set selinux to permissive
     * alphabetical order in BoardConfig.mk
     * update mkbootimg.mk
     * move bump script to separate folder and fix chmod
     * update partitions
     * wipe omni.dependencies
     * fix init scripts
    
    thanks to @dadi11 and @ppajda for support!

--- Android.mk
+++ Android.mk
@@ -5,5 +5,11 @@ ifeq ($(TARGET_DEVICE),d855)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.recovery.g3.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := RECOVERY_EXECUTABLES
+LOCAL_SRC_FILES         := recovery/init.recovery.g3.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
 
 endif
