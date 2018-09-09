ba2481454663aea16398b4c8c4e883dfd3652285
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-09-09 17:55:02 +0200

    LiquidRemix for g3ds

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,4 @@
-#!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-set -e
-
-export DEVICE=g3ds
-export DEVICE_COMMON=g3-common
-export VENDOR=lge
-
-export DEVICE_BRINGUP_YEAR=2016
-
-./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/liquid.mk
--- liquid.mk
+++ liquid.mk
@@ -18,15 +18,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common LiquidRemix stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Inherit from g3ds device
 $(call inherit-product, device/lge/g3ds/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := g3ds
-PRODUCT_NAME := lineage_g3ds
+PRODUCT_NAME := liquid_g3ds
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := g3ds
 PRODUCT_MANUFACTURER := LGE
